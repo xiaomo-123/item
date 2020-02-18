8db8b409e136b6a5108c8b808c82970572b3c3d5
xiaomo-123 <2763349475@qq.com>
2020-02-18 15:29:16 +0800

    1-远程加载图片回调函数

--- user/user.js
+++ user/user.js
@@ -14,93 +14,18 @@ pic:[]
    */
   onLoad: function (options) {
     var that = this;
-    if (app.globalData.status) {
-      console.log('为真时第二个页面回调');
-      console.log(app.globalData.sourceLoaded);
+    if (app.globalData.status) {    
       this.setData({
         pic: app.globalData.sourceLoaded.arr
       })
     } else {
-      app.sourceLoaded = source => {
-        console.log('为假时第二个页面回调');
-        console.log(source);
-        console.log(app.globalData.status);
+      app.sourceLoaded = source => {       
         this.setData({
             pic: source.arr
         })
       }
-    }
-    
-    // app.sourceLoaded = source => {
-    //         console.log(source);
-    //         this.setData({
-    //             pic: source.arr
-    //         })
-    //     }
-//     var that=this;
-//  var redata = wx.getStorageSync('data')
-//     that.setData({
-//      pic: redata.arr
-// })
-    // console.log(app.globalData.sourceLoaded);
-    
-    //   this.setData({
-    //     pic: app.globalData.sourceLoaded.arr
-    //   })
+    }   
 
-// var loaddata = wx.getStorageSync('data')
-//     that.setData({
-//      pic: loaddata.arr
-// })
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
--- utils/common.js
+++
@@ -1,4 +0,0 @@
-function sayhello(name){
-	console.log('hello world',name);
-}
-module.exports= {sayhello};
