b434e51381560a68c15a193b702bc7649c4c512a
xiaomo-123 <2763349475@qq.com>
2020-02-26 20:28:36 +0800

    3-会画兔-带模板远程图片

--- user/user.js
+++ user/user.js
@@ -13,10 +13,9 @@ pic:[]
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
- 
-app.PromiseGetData().then(function(res){ 
-       that.setData({
+    var that = this; 
+     app.PromiseGetData().then(function(res){ 
+      that.setData({
             pic: res.arr
         })
     })
